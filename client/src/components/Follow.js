199e87bef089e57b6ca2e96b9ada55d35a08be50
Tim Quattrochi <timq82@gmail.com>
2023-05-16 21:21:58 -0400

    show error if user already follows

--- client/src/components/Follow.js
+++ client/src/components/Follow.js
@@ -1,10 +1,11 @@
 import axios from "../hooks/useAxios";
 import { Button } from "react-bootstrap";
 
-const Follow = ({ handleFollow }) => {
+const Follow = ({ handleFollow, error }) => {
   return (
     <div>
       <Button onClick={handleFollow}>Follow</Button>
+      <div style={{ color: "red" }}>{error && error}</div>
     </div>
   );
 };

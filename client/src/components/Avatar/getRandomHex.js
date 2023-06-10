e7d7da416148c006e336a338b60e300019d58fc4
Tim Quattrochi <timq82@gmail.com>
2023-06-10 16:31:23 -0400

    added option for skin tones to be more realistic instead of random hex

--- client/src/components/Avatar/getRandomHex.js
+++ client/src/components/Avatar/getRandomHex.js
@@ -7,3 +7,14 @@ export function getRandomHexNumber() {
   }
   return hexNumber;
 }
+
+export function getRandomSkinToneHex() {
+  const skinTones = [
+    "#FCE0C7", "#F8D9C1", "#F5CBA7", // Light Skin Tones
+    "#E0A984", "#D29B76", "#C48763", // Medium Skin Tones
+    "#8C583C", "#774D3A", "#633D2E"  // Dark Skin Tones
+  ];
+
+  const randomIndex = Math.floor(Math.random() * skinTones.length);
+  return skinTones[randomIndex];
+}

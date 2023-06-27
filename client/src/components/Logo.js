b5dab875399b678f099f047f58c4b23bea6a9fbf
Tim Quattrochi <timq82@gmail.com>
2023-06-27 10:11:17 -0400

    wrapped Logo in Link to homepage

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,10 +1,17 @@
 import React from "react";
- 
+import { Link } from "react-router-dom";
+
 const Logo = () => {
   return (
-    <div className="logo-container">
-      <img className="logo" src={require("client/public/tgram.png")} alt="TGram"/>
-    </div>
+    <Link to={"/"}>
+      <div className="logo-container">
+        <img
+          className="logo"
+          src={require("client/public/tgram.png")}
+          alt="TGram"
+        />
+      </div>
+    </Link>
   );
 };
 
